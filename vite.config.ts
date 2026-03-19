1c71e9ce63d9bb16e305fb474f2f6a4ebedb2506
KGFCH2 <babinbid05@gmail.com>
2026-03-20 02:14:39 +0530

    Configured Vite with React and proxy

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(() => ({
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+    proxy: {
+      "/api-notion": {
+        target: "https://api.notion.com/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api-notion/, ""),
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Proxying request to:', proxyReq.protocol + '//' + proxyReq.host + proxyReq.path);
+          });
+        }
+      },
+      "/api-openai": {
+        target: "https://api.openai.com/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api-openai/, ""),
+      },
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
